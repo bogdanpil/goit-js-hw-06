e44fc86320f24d2393982428595cd480f5984b2e
Bogdan <pilipaka@gmail.com>
2025-05-11 23:18:21 +0300

    done all 3 tasks

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,32 @@
-//Width of the element
-function getElementWidth(content, padding, border) {
-    const contentWidth = parseFloat(content);
-    const paddingWidth = parseFloat(padding);
-    const borderWidth = parseFloat(border);
-    return contentWidth + paddingWidth * 2 + borderWidth * 2;
+//Конструктор рядків
+class StringBuilder {
+    #value;
+    constructor(initialValue) {
+        this.#value = initialValue;
+    }
+
+    getValue() {
+        return this.#value;
+    }
+
+    padEnd(str) {
+        this.#value = this.#value + str;
+    }
+
+    padStart(str) {
+        this.#value = str + this.#value;
+    }
+
+    padBoth(str) {
+        this.#value = str + this.#value + str;
+    }
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^=
